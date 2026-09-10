0995bd8d22cf7753fb3e290a8882dccb8288c6e8
tolunay Caglayan <66451808+Tolunay-c@users.noreply.github.com>
2026-09-10 20:09:27 +0300

    update
    update vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 chdir(__DIR__ . '/../v-1');
-require __DIR__ . '/../v-1/router.php';
+require __DIR__ . '/../v-1/index.php';
--- v-1/includes/header.php
+++ v-1/includes/header.php
@@ -1,11 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-chdir(__DIR__ . '/../v-1');
-require __DIR__ . '/../v-1/index.php';
 
 
 /** @var string $sayfa_basligi */
