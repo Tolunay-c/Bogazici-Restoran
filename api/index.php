17678e251c941983d246a269b681f673d52c9f02
tolunay Caglayan <66451808+Tolunay-c@users.noreply.github.com>
2026-09-10 20:12:37 +0300

    Update index.php
    linkler çalışmıyor sayfalar gelmiyor

--- api/index.php
+++ api/index.php
@@ -1,7 +1,23 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 chdir(__DIR__ . '/../v-1');
-require __DIR__ . '/../v-1/index.php';
+
+// İstek yapılan dosya yolunu al (örn: /iletisim.php -> iletisim.php)
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$file = ltrim($uri, '/');
+
+// Eğer kök dizin ise veya dosya belirtilmemişse index.php aç
+if ($file === '' || $file === '/') {
+    require __DIR__ . '/../v-1/index.php';
+    exit;
+}
+
+// Güvenli dosya kontrolü: İstenen PHP dosyası v-1 altında var mı?
+$target = realpath(__DIR__ . '/../v-1/' . $file);
+$baseDir = realpath(__DIR__ . '/../v-1');
+
+if ($target && str_starts_with($target, $baseDir) && file_exists($target) && !is_dir($target)) {
+    require $target;
+} else {
+    // Bulunamazsa index.php'ye yönlendir
+    require __DIR__ . '/../v-1/index.php';
+}
