c8a548eec0f63f5852c1b54cd6308f4b48f5a47e
tolunay Caglayan <66451808+Tolunay-c@users.noreply.github.com>
2026-09-10 21:31:19 +0300

    update
    update

--- api/index.php
+++ api/index.php
@@ -1,23 +1,39 @@
 <?php
-chdir(__DIR__ . '/../v-1');
-
-// İstek yapılan dosya yolunu al (örn: /iletisim.php -> iletisim.php)
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$file = ltrim($uri, '/');
+$uri = trim($uri, '/');
 
-// Eğer kök dizin ise veya dosya belirtilmemişse index.php aç
-if ($file === '' || $file === '/') {
-    require __DIR__ . '/../v-1/index.php';
+// İstek /v-2 ile mi başlıyor?
+if ($uri === 'v-2' || str_starts_with($uri, 'v-2/')) {
+    $subPath = preg_replace('#^v-2/?#', '', $uri);
+    $baseDir = realpath(__DIR__ . '/../v-2');
+    chdir($baseDir);
+
+    if ($subPath === '' || $subPath === 'index.php') {
+        require $baseDir . '/index.php';
+        exit;
+    }
+
+    $target = realpath($baseDir . '/' . $subPath);
+    if ($target && str_starts_with($target, $baseDir) && file_exists($target) && !is_dir($target)) {
+        require $target;
+    } else {
+        require $baseDir . '/index.php';
+    }
     exit;
 }
 
-// Güvenli dosya kontrolü: İstenen PHP dosyası v-1 altında var mı?
-$target = realpath(__DIR__ . '/../v-1/' . $file);
+// Geri kalan tüm istekler v-1 için çalışır
 $baseDir = realpath(__DIR__ . '/../v-1');
+chdir($baseDir);
 
+if ($uri === '' || $uri === 'index.php') {
+    require $baseDir . '/index.php';
+    exit;
+}
+
+$target = realpath($baseDir . '/' . $uri);
 if ($target && str_starts_with($target, $baseDir) && file_exists($target) && !is_dir($target)) {
     require $target;
 } else {
-    // Bulunamazsa index.php'ye yönlendir
-    require __DIR__ . '/../v-1/index.php';
+    require $baseDir . '/index.php';
 }
